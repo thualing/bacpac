3fcdf09a7ce8e2c08bfd6edb4f82f8c383946bcc
shawnsuarez <shawn.darrel.suarez@gmail.com>
2017-04-05 21:21:17 -0700

    Moved new code to respective files
    Moved new css code to bacpac-upload.css.
    Moved new js code to bacpac-upload.js

--- BacPac/assets/js/pages/bacpac-upload.js
+++ BacPac/assets/js/pages/bacpac-upload.js
@@ -3,3 +3,157 @@ $(document).ready(function(){
 	$(".dropzone").dropzone();
 });
 
+// Initialize Firebase
+// Currently using Shawn's FB Project, Replace with official BacPac FB Project
+var config = {
+	apiKey: "AIzaSyA8_jwESeKlQsO5VsIn0CudKRhNxU-Q1eg",
+	authDomain: "bacpactest.firebaseapp.com",
+	databaseURL: "https://bacpactest.firebaseio.com",
+	projectId: "bacpactest",
+	storageBucket: "bacpactest.appspot.com",
+	messagingSenderId: "7980278317"
+};
+firebase.initializeApp(config);
+
+var storageRef = firebase.storage().ref();
+
+//New Drag & Drop scripts
+//Credit: https://hurlatunde.github.io/articles/2017-01/multiple-drag-and-drop-file-uploading-to-firebase-storage
+var obj = $("#drop-zone");
+obj.on('dragenter', function (e) {
+	e.stopPropagation();
+    e.preventDefault();
+    $(this).css('border', '2px solid #0B85A1');
+});
+
+obj.on('dragover', function (e) {
+    e.stopPropagation();
+    e.preventDefault();
+});
+
+obj.on('drop', function (e) {
+    
+    $(this).css('border', '2px dotted #0B85A1');
+    e.preventDefault();
+    var files = e.originalEvent.dataTransfer.files;
+    
+    //We need to send dropped files to Server
+    handleFileUpload(files, obj);
+});
+    
+$(document).on('dragenter', function (e) {
+    e.stopPropagation();
+    e.preventDefault();
+});
+
+$(document).on('dragover', function (e) {
+    e.stopPropagation();
+    e.preventDefault();
+    obj.css('border', '2px dotted #0B85A1');
+});
+
+$(document).on('drop', function (e) {
+    e.stopPropagation();
+    e.preventDefault();
+});
+    
+// automatically submit the form on file select
+$('#drop-zone-file').on('change', function (e) {
+    var files = $('#drop-zone-file')[0].files;
+    handleFileUpload(files, obj);
+});
+    
+function handleFileUpload(files, obj) {
+    for (var i = 0; i < files.length; i++) {
+        var fd = new FormData();
+        fd.append('file', files[i]);
+    
+        console.log(files[i]);
+        fireBaseImageUpload({
+            'file': files[i],
+            'path': "files/"
+        }, function (data) {
+            //console.log(data);
+            if (!data.error) {
+                if (data.progress) {
+                    // progress update to view here
+                }
+                if (data.downloadURL) {
+                    // update done
+                    // download URL here "data.downloadURL"
+                }
+            } else {
+                console.log(data.error + ' Firebase image upload error');
+            }
+        });
+    }
+};
+    
+function fireBaseImageUpload(parameters, callBackData) {
+
+    // expected parameters to start storage upload
+    var file = parameters.file;
+    var path = parameters.path;
+    var name;
+
+    //just some error check
+    if (!file) { callBackData({error: 'file required to interact with Firebase storage'}); }
+    if (!path) { callBackData({error: 'Node name required to interact with Firebase storage'}); }
+
+    var metaData = {'contentType': file.type};
+    var arr = file.name.split('.');
+    var fileSize = formatBytes(file.size); // get clean file size (function below)
+    var fileType = file.type;
+    var n = file.name;
+
+    // generate random string to identify each upload instance
+    name = generateRandomString(12); //(location function below)
+
+    //var fullPath = path + '/' + name + '.' + arr.slice(-1)[0];
+    var fullPath = path + '/' + file.name;
+
+    var uploadFile = storageRef.child(fullPath).put(file, metaData);
+
+    // first instance identifier
+    callBackData({id: name, fileSize: fileSize, fileType: fileType, fileName: n});
+
+    uploadFile.on('state_changed', function (snapshot) {
+        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        progress = Math.floor(progress);
+        callBackData({
+            progress: progress,
+            element: name,
+            fileSize: fileSize,
+            fileType: fileType,
+            fileName: n});
+    }, function (error) {
+        callBackData({error: error});
+    }, function () {
+        var downloadURL = uploadFile.snapshot.downloadURL;
+        callBackData({
+            downloadURL: downloadURL,
+            element: name,
+            fileSize: fileSize,
+            fileType: fileType,
+            fileName: n});
+    });
+}
+
+function generateRandomString(length) {
+    var chars = "abcdefghijklmnopqrstuvwxyz";
+    var pass = "";
+    for (var x = 0; x < length; x++) {
+        var i = Math.floor(Math.random() * chars.length);
+        pass += chars.charAt(i);
+    }
+    return pass;
+}
+
+function formatBytes(bytes, decimals) {
+    if (bytes == 0) return '0 Byte';
+    var k = 1000;
+    var dm = decimals + 1 || 3;
+    var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    var i = Math.floor(Math.log(bytes) / Math.log(k));
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+}
