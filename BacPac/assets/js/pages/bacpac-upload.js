696ad9b04d331ab3ed56aca008df634ec5ccb40c
shawnsuarez <shawn.darrel.suarez@gmail.com>
2017-04-09 16:06:38 -0700

    Added Data Transfer.
    Data transfer code added.

--- BacPac/assets/js/pages/bacpac-upload.js
+++ BacPac/assets/js/pages/bacpac-upload.js
@@ -10,7 +10,13 @@ var config = {
 firebase.initializeApp(config);
 
 var storageRef = firebase.storage().ref();
-var databaseRef = firebase.database().ref();
+var databaseRef = firebase.database();
+var auth = firebase.auth();
+
+// Firebase Authentication Safeguard
+var user = null;
+readSessionData(getParameterByName("uid"));
+
 
 //New Drag & Drop scripts
 //Credit: https://hurlatunde.github.io/articles/2017-01/multiple-drag-and-drop-file-uploading-to-firebase-storage
@@ -153,4 +159,36 @@ function formatBytes(bytes, decimals) {
     var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
     var i = Math.floor(Math.log(bytes) / Math.log(k));
     return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+}
+
+/* Action: Logout Button click */
+$("#logoutBtn").on("click", function(){
+ auth.signOut().then(function(){
+     console.log("Signing Out");
+     window.location = ("bacpac-login.html");
+ }).catch(function(error){
+     console.log(error);
+ });
+});
+
+
+/* Utility: Query String Parameter Retriever */
+function getParameterByName(name, url) {
+    if (!url) {
+     url = window.location.href;
+    }
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+/* Utility: readSessionData */
+function readSessionData(userId) {
+ database.ref("/sessions/" + userId).once("value").then( function(snapshot){
+     user = snapshot.val();
+     console.log("Current User: " + JSON.stringify(user));
+ });
 }
