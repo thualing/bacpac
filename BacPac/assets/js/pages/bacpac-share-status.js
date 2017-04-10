ca62c9a796fd83d9e65b99e83c05849b6e194048
alti21 <altemush@gmail.com>
2017-04-09 21:31:48 -0700

    Sharing Status
    The uploaded files' info will appear here when shared. DEVELOPMENT STILL
    IN PROGRESS

--- BacPac/assets/js/pages/bacpac-share-status.js
+++ BacPac/assets/js/pages/bacpac-share-status.js
@@ -1,10 +1,16 @@
-$(document).ready(function(){
+$(document).ready(function () {
 	/* ---------- Datable ---------- */
 	$('.datatable').dataTable({
-		"sDom": "<'row'<'col-lg-6'l><'col-lg-6'f>r>t<'row'<'col-lg-12'i><'col-lg-12 center'p>>",
-		"sPaginationType": "bootstrap",
+	    "sDom": "<'row'<'col-lg-6'l><'col-lg-6'f>r>t<'row'<'col-lg-12'i><'col-lg-12 center'p>>",
+	    "sPaginationType": "bootstrap",
+	   
 		"oLanguage": {
 			"sLengthMenu": "_MENU_ records per page"
 		}
+
 	});
+	$("#btn1").on("click", function () {
+	    alert("I Pushed this button!");
+	});
+
 });
