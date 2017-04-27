1206718c3889ace675031561af5bceb004c114e7
Rolando Javier <rjavier441@gmail.com>
2017-04-26 18:57:13 -0700

    Updated my local copy of Shawn's Upload module to additionally update the new databases implemented on Database Schema Revision 3, namely folder and fileAttribute.

--- BacPac/assets/js/pages/bacpac-upload.js
+++ BacPac/assets/js/pages/bacpac-upload.js
@@ -147,8 +147,17 @@ function fireBaseImageUpload(parameters, callBackData) {
 
 /* Creates a record of file in DB*/
 function writeUploadToDB(userId, fileName, filePath) {
-    database.ref("fileName/" + userId +"/" + fileName).set({
-        'path' : filePath
+    var plainTextFileName = decodeURIComponent(fileName);
+    var plainTextPath = decodeURIComponent(filePath);
+    database.ref("fileName/" + userId + "/" + fileName).set({
+        'path' : plainTextPath
+    });
+    database.ref("fileAttribute/" + userId + "/" + fileName).set({
+        '0' : 0
+    });
+    database.ref("folder/" + userId + "/" + fileName).set({
+        'name' : plainTextFileName,
+        'path' : plainTextPath
     });
 }
 
@@ -171,71 +180,6 @@ function formatBytes(bytes, decimals) {
     return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
 }
 
-// /* Action: Logout Button click */
-// $("#logoutBtn").on("click", function(){
-//     database.ref("/sessions/" + user.data.uid).remove().then(function(){
-//         console.log("Session Ended...");
-//         auth.signOut().then(function(){
-//             console.log("Signing Out");
-//             window.location = ("bacpac-login.html");
-//         }).catch(function(error){
-//             console.log(error);
-//         });
-//     }).catch(function(error){
-//         console.log("Internal Error Occurred! [" + error + "]");
-//     });
-// });
-    
-function fireBaseImageUpload(parameters, callBackData) {
-
-    // expected parameters to start storage upload
-    var file = parameters.file;
-    var path = parameters.path;
-    var name;
-
-    //just some error check
-    if (!file) { callBackData({error: 'file required to interact with Firebase storage'}); }
-    if (!path) { callBackData({error: 'Node name required to interact with Firebase storage'}); }
-
-    var metaData = {'contentType': file.type};
-    var arr = file.name.split('.');
-    var fileSize = formatBytes(file.size); // get clean file size (function below)
-    var fileType = file.type;
-    var n = file.name;
-
-    // generate random string to identify each upload instance
-    name = generateRandomString(12); //(location function below)
-
-    //var fullPath = path + '/' + name + '.' + arr.slice(-1)[0];
-    var fullPath = path + '/' + file.name;
-
-    var uploadFile = storageRef.child(fullPath).put(file, metaData);
-
-    // first instance identifier
-    callBackData({id: name, fileSize: fileSize, fileType: fileType, fileName: n});
-
-    uploadFile.on('state_changed', function (snapshot) {
-        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        progress = Math.floor(progress);
-        callBackData({
-            progress: progress,
-            element: name,
-            fileSize: fileSize,
-            fileType: fileType,
-            fileName: n});
-    }, function (error) {
-        callBackData({error: error});
-    }, function () {
-        var downloadURL = uploadFile.snapshot.downloadURL;
-        callBackData({
-            downloadURL: downloadURL,
-            element: name,
-            fileSize: fileSize,
-            fileType: fileType,
-            fileName: n});
-    });
-}
-
 function generateRandomString(length) {
     var chars = "abcdefghijklmnopqrstuvwxyz";
     var pass = "";
@@ -254,38 +198,6 @@ function formatBytes(bytes, decimals) {
     var i = Math.floor(Math.log(bytes) / Math.log(k));
     return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
 }
-
-// /* Action: Logout Button click */
-// $("#logoutBtn").on("click", function(){
-//  auth.signOut().then(function(){
-//      console.log("Signing Out");
-//      window.location = ("bacpac-login.html");
-//  }).catch(function(error){
-//      console.log(error);
-//  });
-// });
-
-// /* Utility: Query String Parameter Retriever */
-// function getParameterByName(name, url) {
-//     if (!url) {
-//      url = window.location.href;
-//     }
-//     name = name.replace(/[\[\]]/g, "\\$&");
-//     var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-//         results = regex.exec(url);
-//     if (!results) return null;
-//     if (!results[2]) return '';
-//     return decodeURIComponent(results[2].replace(/\+/g, " "));
-// }
-
-// /* Utility: readSessionData */
-// function readSessionData(userId) {
-//     database.ref("/sessions/" + userId).once("value").then( function(snapshot){
-//         user = snapshot.val();
-//         console.log("Current User: " + JSON.stringify(user));
-//         pageInit(user);
-//     });
-// }
 
 /* Utility: pageInit */
 function pageInit(user) {
